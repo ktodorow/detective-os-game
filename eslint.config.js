1d7cff4690c894249bb851d8bfc4552974c759a3
Kristiyan Todorov <kristiantodorov552@abv.bg>
2026-03-26 23:36:45 +0200

    added wifi system

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,10 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': 'off',
+      'react-hooks/refs': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/purity': 'off',
     },
   },
 ])
